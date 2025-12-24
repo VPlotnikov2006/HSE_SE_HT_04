9e069fef3d936b81a63261c0843cf3cf0f7cfd63
Plotnikov V.D. <pltnikovqwerty@gmail.com>
2025-12-24 10:20:23 +0300

    Orders Service - Infrastrucutre - DI

---
+++ OrdersService/OrdersService.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrdersService.Application.Interfaces.Messaging;
+using OrdersService.Application.Interfaces.Repositories;
+using OrdersService.Infrastructure.Daemons;
+using OrdersService.Infrastructure.Messaging.Kafka;
+using OrdersService.Infrastructure.Persistence.DbContexts;
+using OrdersService.Infrastructure.Persistence.Repositories;
+using OrdersService.Infrastructure.Persistence.UnitOfWork;
+
+namespace OrdersService.Infrastructure.DependencyInjection;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddOrdersServiceInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<OrdersDbContext>(options =>
+        {
+            options.UseNpgsql(
+                configuration.GetConnectionString("OrdersDb"),
+                npgsql =>
+                {
+                    npgsql.MigrationsAssembly("OrdersService.Infrastructure");
+                }
+            );
+        });
+
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IOutboxRepository, OutboxRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddSingleton<IMessageProducer, KafkaMessageProducer>();
+        services.AddSingleton<IMessageConsumer, KafkaMessageConsumer>();
+
+        services.AddHostedService<OutboxPublisherDaemon>();
+        services.AddHostedService<PaymentStatusConsumerDaemon>();
+
+        return services;
+    }
+}
